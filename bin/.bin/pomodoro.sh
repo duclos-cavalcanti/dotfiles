2a2d678cb768b7c9ee1132f3307836c2ca6b1e5c
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-09-12 15:04:04 +0200

    Update

--- bin/.bin/pomodoro.sh
+++ bin/.bin/pomodoro.sh
@@ -2,18 +2,21 @@
 
 ICON_PATH="/home/duclos/.config/awesome/theme/icons/clock.png"
 
-while [ 1 ]; do
+i=0
+while :; do
     notify-send -i ${ICON_PATH} \
                 -t 2000 \
                 "Work Time!"
 
-    for ((i=0; i++; i<9)); do
+    for i in $(seq 1 9); do
         sleep 5m
         part=$((i + 1))
         notify-send -i ${ICON_PATH} \
                     -t 1000 \
                     "${part} out of 9 working intervals is done!"
     done
+
+    exit 1
 
     notify-send -i ${ICON_PATH} \
                 -t 2000 \

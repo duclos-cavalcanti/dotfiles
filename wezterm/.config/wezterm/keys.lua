0466f3ce955f52eebc24f55abe8188e55ec51e90
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-04-09 01:59:18 -0300

    Update

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -35,8 +35,8 @@ M.base = {
 	{ key = "h", mods = "ALT", action = wezterm.action({ MoveTabRelative = -1 }) },
 	{ key = "l", mods = "ALT", action = wezterm.action({ MoveTabRelative = 1 }) },
 
-	{ key = "raw:19", mods = "LEADER", action = wezterm.action({ SwitchWorkspaceRelative = -1 }) },
-	{ key = "(", mods = "LEADER", action = wezterm.action({ SwitchWorkspaceRelative = 1 }) },
+	{ key = "0", mods = "LEADER", action = wezterm.action({ SwitchWorkspaceRelative = -1 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ SwitchWorkspaceRelative = 1 }) },
 
 	{ key = "=", mods = "CTRL", action = "ResetFontSize" },
 	{ key = "+", mods = "CTRL", action = "IncreaseFontSize" },
@@ -59,7 +59,7 @@ M.base = {
       },
     },
     { -- rename workspace
-      key = 'raw:13', -- shift + 4 = $
+      key = '4',
       mods = 'LEADER',
       action = wezterm.action.PromptInputLine {
         description = 'Enter new name for workspace',
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -32,7 +32,7 @@ local config = {
 
     enable_scroll_bar = true,
     use_fancy_tab_bar = false,
-    tab_bar_at_bottom  = true,
+    tab_bar_at_bottom  = false,
 
     hide_tab_bar_if_only_one_tab = false,
 

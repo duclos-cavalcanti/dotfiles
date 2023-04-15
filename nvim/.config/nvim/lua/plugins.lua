6bdb0ee9404f6196a58e971312a8c395273e5b8d
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-04-14 23:49:46 -0300

    Update

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -114,6 +114,7 @@ return require('packer').startup(function(use)
     use { 'nvim-lualine/lualine.nvim',
           requires = { 'kyazdani42/nvim-web-devicons', opt = true }
         }
+    
 
     -- view colors in nvim
     use { 'norcalli/nvim-colorizer.lua',

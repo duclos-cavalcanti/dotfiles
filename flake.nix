9ad2e4ae2c8d06b04e3f9834d0d9662d64e293c5
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-08-10 12:50:12 -0300

    Update

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
-{   config, pkgs, ... }:
-
 {
-    imports = [
-        ./modules/base.nix 
-        ./modules/hardware.nix
-        ./modules/x11.nix
-        ./modules/packages.nix
-    ];
+    description = "Daniel Cavalcanti's NixOS configuration";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    outputs = { self, nixpkgs }: {
+        nixosConfigurations.myhostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+            ./modules/base.nix 
+            ./modules/hardware.nix
+            ./modules/x11.nix
+            ./modules/packages.nix
+        ];
+    };
+  };
 }

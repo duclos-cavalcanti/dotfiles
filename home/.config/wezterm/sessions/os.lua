15400e703d33654571aad8b64fdedde398718a10
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-09-19 16:46:17 -0400

    Update

---
+++ home/.config/wezterm/sessions/os.lua
@@ -0,0 +1,19 @@
+
+return {
+    name = "work",
+    dir = os.getenv('HOME') .. "/Documents/work/os-nyu",
+    tabs = {
+        {
+            name = "readme",
+            cmds = {
+                "nvim README.md"
+            },
+        },
+        {
+            name = "bash",
+            cmds = {
+                "ls"
+            },
+        },
+    }
+}

f68b81585a249ec0c3f21b156e410836a08a3b06
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-10-07 16:25:11 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "Daniel Cavalcanti's NixOS configuration";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
@@ -13,20 +13,20 @@
         nixosConfigurations = {
             hostname = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
+
                 modules = [
                     ./modules/base.nix 
                     ./modules/hardware.nix
                     ./modules/x11.nix
                     ./modules/packages.nix
-
-                    # home-manager.nixosModules.home-manager {
-                    #   home-manager.useGlobalPkgs = true;
-                    #   home-manager.useUserPackages = true;
-                    #   home-manager.users.jdoe = import ./home.nix;
-                    #
-                    # }
                 ];
-            };
-        };
+
+                home-manager.nixosModules.home-manager {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.duclos = import ./home.nix;
+                }
+            }
+        }
     };
 }

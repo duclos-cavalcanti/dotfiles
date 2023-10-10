5fad2ddfeb0850b4134dffaf8c307ef4052e8d09
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-10-10 13:31:42 -0400

    Update

---
+++ stow.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# Personal Stow script of my dotiles onto a new machine.
+
+FOLDERS=(bash
+         awesome
+         picom
+         wezterm
+         nvim
+         bin
+         git
+         X11)
+
+usage() {
+  printf "USAGE: syntax: ./install.sh <options>
+  OPTIONS:
+    -i | --install:
+    -r | --reinstall:
+    -u | --uninstall:
+    -s | --system:
+  "
+}
+
+check() {
+    local program="$1"
+    if ! command -v ${program} &>/dev/null; then
+        printf "${program} is not installed!\n"
+        exit 1
+    fi
+}
+
+main() {
+    local target=
+    local uninstall="stow -Dvt $HOME"
+    local install="stow -Svt $HOME"
+
+    if [ $# -lt 1 ]; then
+      usage
+      exit 1
+    fi
+
+    while [[ $# -gt 0 ]]; do
+       case $1 in
+            -s|--system)
+                pushd home
+                for f in ${FOLDERS[@]}; do
+                  if [ -d $f ]; then
+                    $install --adopt $f
+                  fi
+                done
+                popd
+                exit 0
+                ;;
+
+            -i|--install)
+                shift
+                target="${1}"
+                if [ -d $target ]; then
+                    $install $target
+                fi
+                shift
+                ;;
+
+            -r|--reinstall)
+                shift
+                target="${1}"
+                if [ -d $target ]; then
+                    $install --restow $target
+                fi
+                shift
+                ;;
+
+            -u|--uninstall)
+                shift
+                target="${1}"
+                if [ -d $target ]; then
+                    $uninstall $target
+                fi
+                shift
+                ;;
+
+            -h|--help)
+                usage
+                exit 0
+                ;;
+
+            *)
+                usage
+                exit 1
+                ;;
+       esac
+    done
+}
+
+check stow
+main "$@"

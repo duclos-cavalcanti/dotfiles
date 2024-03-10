8f3814cfd32537935b04e2259674d87ff0301a9c
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-03-10 18:51:47 -0400

    Update

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -9,11 +9,3 @@ require("theme")
 
 -- vim.opt.runtimepath:append("$HOME/.config/nvim/pack/plugins/start/example")
 -- require("example").setup({})
-
--- treesitter issue 
--- https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
-
--- local args = vim.fn.argv()
--- if args[1] == "g" then
---     vim.cmd[["execute ':Neogit'"]]
--- end
